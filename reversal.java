e06bd8b849c0d7f5afb06338874790ac8c1e4856
him1208 <60592955+him1208@users.noreply.github.com>
2020-12-07 15:51:07 +0530

    Create reversal.java

---
+++ reversal.java
@@ -0,0 +1,30 @@
+public class WordsReversal {
+
+	public static String reverseWords(String str) {
+		
+		//Declare a new StringBuilder object
+		StringBuilder sb = new StringBuilder(str);
+		String revStr = sb.reverse().toString();//contains "uoy era woH"
+
+		String[] words = revStr.split(" ");
+		StringBuilder reverse = new StringBuilder();
+		for (String temp : words) {
+			sb = new StringBuilder(temp);
+			reverse.append(sb.reverse()+" ");
+		}
+		
+		//to remove last space
+		reverse.deleteCharAt(reverse.length()-1);
+		
+		return reverse.toString();
+	}
+
+	public static void main(String[] args) {
+
+		String str = "How are you";
+
+		System.out.println("Original String : "+str);
+		System.out.println("Words reversed : "+reverseWords(str));
+
+	}
+}
